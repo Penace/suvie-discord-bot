c99a21b844db4cb0626540e476f3e30e7e52ec3f
Penace <penacedan@gmail.com>
2025-04-13 18:41:28 +0300

    🚀 Deploy to GitHub Pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Landing from "./pages/Landing.jsx";
+import LandingPage from "./pages/Landing";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Landing />
+    <LandingPage />
   </React.StrictMode>
 );
