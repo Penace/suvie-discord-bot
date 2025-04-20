6c2bb09124c2aa94da1b2967bdc75272070b99e1
Penace <penacedan@gmail.com>
2025-04-20 12:02:36 +0300

    added Docs page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import LandingPage from "./LandingPage.jsx";
+import DocsPage from "./DocsPage.jsx"; // ⬅️ New component for /docs
 import KoFiWidget from "./components/KoFiWidget.jsx";
 import ThemeToggle from "./components/ThemeToggle.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <LandingPage />
-    <KoFiWidget />
-    <ThemeToggle />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/docs" element={<DocsPage />} />
+      </Routes>
+
+      {/* Global Widgets (always rendered) */}
+      <KoFiWidget />
+      <ThemeToggle />
+    </BrowserRouter>
   </StrictMode>
 );
